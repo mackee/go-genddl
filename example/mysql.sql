d0750988333e5315836b2c60d2be4f633bff7941
Konboi <ryosuke.yabuki@gmail.com>
2015-06-23 16:40:58 +0900

    cd example && go generate

--- example/mysql.sql
+++ example/mysql.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+    `user` INTEGER unsigned NOT NULL,
+    `name` VARCHAR(255) NOT NULL UNIQUE,
+    `user_id` INTEGER unsigned NOT NULL DEFAULT '0',
+    PRIMARY KEY (`user`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
