ae9f08f41a8cd8b18b4f1578544b72cb4e8c3db9
mackee <macopy123@gmail.com>
2022-08-09 15:14:01 +0900

    fix: sql.NullByte is not BLOB NULL. this value consider to be uint8

--- _example/product.go
+++ _example/product.go
@@ -7,17 +7,21 @@ import (
 	"github.com/mackee/go-genddl/index"
 )
 
+// NullUserID is optional UserID
+type NullUserID = sql.NullInt32
+
 // Product is product of user
 //+table: product
 type Product struct {
-	ID          uint32       `db:"id,primarykey,autoincrement"`
-	Name        string       `db:"name"`
-	Type        uint32       `db:"type"`
-	UserID      uint32       `db:"user_id"`
-	Description string       `db:"description,text"`
-	IconImage   sql.NullByte `db:"icon_image"`
-	CreatedAt   time.Time    `db:"created_at"`
-	UpdatedAt   sql.NullTime `db:"updated_at"`
+	ID             uint32        `db:"id,primarykey,autoincrement"`
+	Name           string        `db:"name"`
+	Type           uint32        `db:"type"`
+	UserID         uint32        `db:"user_id"`
+	ReceivedUserID NullUserID    `db:"received_user_id"`
+	Description    string        `db:"description,text"`
+	Size           sql.NullInt16 `db:"size"`
+	CreatedAt      time.Time     `db:"created_at"`
+	UpdatedAt      sql.NullTime  `db:"updated_at"`
 }
 
 func (s Product) _schemaIndex(methods index.Methods) []index.Definition {
